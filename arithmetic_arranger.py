da4ec88d7b645c259713985a9002a1c63027d04d
victorhfsilva <victorhfsilva@protonmail.com>
2022-09-19 10:12:28 -0300

    First Try.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -7,12 +7,36 @@ def is_sum_or_subtraction(problem_str):
     return is_sum
 
 
-def extract_values(problem_str, operator):
-    problem_values_str = problem_str.split(operator)
-
+def raise_values_exceptions(problem_values_str):
     # Raise Exception if after splitting the String there aren't 2 values
     if len(problem_values_str) != 2:
         raise Exception("Error: Syntax Error.")
+
+    value1_str = problem_values_str[0].strip()
+    value2_str = problem_values_str[1].strip()
+
+    # Raise Exception if the values have more than four digits
+    if len(value1_str) > 4 or len(value2_str) > 4:
+        raise Exception("Error: Numbers cannot be more than four digits.")
+
+    # Raise Exception if the values have more than
+    if not (value1_str.isdigit() and value2_str.isdigit()):
+        raise Exception("Error: Numbers must only contain digits.")
+
+
+def calculate_answer(value1, value2, operator):
+    if operator == "+":
+        answer = value1 + value2
+    elif operator == "-":
+        answer = value1 - value2
+    else:
+        raise Exception("Error: Invalid Operator.")
+    return answer
+
+
+def extract_values(problem_str, operator):
+    problem_values_str = problem_str.split(operator)
+    raise_values_exceptions(problem_values_str)
 
     value1_str = problem_values_str[0].strip()
     value1_len = len(value1_str)
@@ -22,12 +46,7 @@ def extract_values(problem_str, operator):
     value2_len = len(value2_str)
     value2 = int(value2_str)
 
-    if operator == "+":
-        answer = value1 + value2
-    elif operator == "-":
-        answer = value1 - value2
-    else:
-        raise Exception("Error: Invalid Operator.")
+    answer = calculate_answer(value1, value2, operator)
     answer_str = str(answer)
     answer_len = len(answer_str)
 
@@ -37,17 +56,21 @@ def extract_values(problem_str, operator):
     return values
 
 
-def print_line_when_line_not_biggest(bigger_value_len, line_value_len, line_value,is_first_value):
-    if is_first_value:
-        print("  ", end="")
+def print_line_when_line_not_biggest(biggest_value_len, line_value_len, line_value,is_second_value,is_sum):
+    if is_second_value:
+        if is_sum:
+            print("+ ", end="")
+        else:
+            print("- ", end="")
     else:
-        print("+ ", end="")
-    for i in range(bigger_value_len - line_value_len):
+        print("  ", end="")
+    for i in range(biggest_value_len - line_value_len):
         print(" ", end="")
     print(line_value, end="    ")
 
 
-def print_first_line(values_list):
+def print_first_line(values_list, is_sum_list):
+    i = 0
     for values in values_list:
         value1 = values[0]["String"]
         value1_len = values[0]["Length"]
@@ -55,15 +78,17 @@ def print_first_line(values_list):
         answer_len = values[2]["Length"]
 
         if value1_len >= value2_len >= answer_len:
-            print("  ", value1, end="    ")
-        elif value2_len >= value1_len and value2_len >= answer_len:
-            print_line_when_line_not_biggest(value2_len, value1_len, value1, True)
+            print(" ", value1, end="    ")
+        elif value2_len >= value1_len >= answer_len:
+            print_line_when_line_not_biggest(value2_len, value1_len, value1, False, is_sum_list[i])
         else:
-            print_line_when_line_not_biggest(answer_len, value1_len, value1, True)
+            print_line_when_line_not_biggest(answer_len, value1_len, value1, False, is_sum_list[i])
     print()
+    i += 1
 
 
-def print_second_line(values_list):
+def print_second_line(values_list, is_sum_list):
+    i = 0
     for values in values_list:
         value2 = values[1]["String"]
         value1_len = values[0]["Length"]
@@ -71,12 +96,13 @@ def print_second_line(values_list):
         answer_len = values[2]["Length"]
 
         if value2_len >= value1_len >= answer_len:
-            print("+ ", value2, end="    ")
-        elif value1_len >= value2_len and value1_len >= answer_len:
-            print_line_when_line_not_biggest(value1_len, value2_len, value2, False)
+            print("+", value2, end="    ")
+        elif value1_len >= value2_len >= answer_len:
+            print_line_when_line_not_biggest(value1_len, value2_len, value2, True, is_sum_list[i])
         else:
-            print_line_when_line_not_biggest(answer_len, value2_len, value2, False)
+            print_line_when_line_not_biggest(answer_len, value2_len, value2, True, is_sum_list[i])
     print()
+    i += 1
 
 
 def print_third_line(values_list):
@@ -89,30 +115,74 @@ def print_third_line(values_list):
         print("--", end="")
         for i in range(max_len):
             print("-", end="")
-        print("    ",end="")
+        print("    ", end="")
     print()
 
 
-class ArithmeticArranger:
+def print_answers(values_list, is_sum_list):
+    i = 0
+    for values in values_list:
+        answer = values[2]["String"]
+        value1_len = values[0]["Length"]
+        value2_len = values[1]["Length"]
+        answer_len = values[2]["Length"]
 
-    def __init__(self, problems):
-        # Raise Exception if too many problems are supplied to the function
-        if len(problems) > 5:
-            raise Exception("Error: Too many problems.")
+        if answer_len >= value1_len >= value2_len:
+            print(" ", answer, end="    ")
+        elif value1_len >= value2_len >= answer_len:
+            print_line_when_line_not_biggest(value1_len, answer_len, answer, False, is_sum_list[i])
+        else:
+            print_line_when_line_not_biggest(value2_len, answer_len, answer, False, is_sum_list[i])
+    print()
+    i += 1
 
-        is_sum_list = list()
-        values_list = list()
-        for problem_str in problems:
-            is_sum = is_sum_or_subtraction(problem_str)
-            is_sum_list.append(is_sum)
 
-            if is_sum:
-                values = extract_values(problem_str, "+")
-                values_list.append(values)
+def get_problems(problems):
+    # Raise Exception if too many problems are supplied to the function
+    if len(problems) > 5:
+        raise Exception("Error: Too many problems.")
 
-        print(values_list)
-        print_first_line(values_list)
-        print_second_line(values_list)
-        print_third_line(values_list)
+    is_sum_list = list()
+    values_list = list()
+    for problem_str in problems:
+        is_sum = is_sum_or_subtraction(problem_str)
+        is_sum_list.append(is_sum)
 
-        # TO-DO: Discover why there is two spaces after the first sum operator
+        if is_sum:
+            values = extract_values(problem_str, "+")
+        else:
+            values = extract_values(problem_str, "-")
+        values_list.append(values)
+    return [values_list, is_sum_list]
+
+
+def print_problems(problems):
+
+    problems_list = get_problems(problems)
+    values_list = problems_list[0]
+    is_sum_list = problems_list[1]
+
+    print_first_line(values_list, is_sum_list)
+    print_second_line(values_list, is_sum_list)
+    print_third_line(values_list)
+
+
+def arithmetic_arranger(*args):
+    if len(args) == 1 and isinstance(args[0], list):
+        problems = args[0]
+        problems_list = get_problems(problems)
+        values_list = problems_list[0]
+        is_sum_list = problems_list[1]
+        print_problems(problems)
+    elif len(args) == 2 and isinstance(args[1], bool):
+        problems = args[0]
+        check_answers = args[1]
+        problems_list = get_problems(problems)
+        values_list = problems_list[0]
+        is_sum_list = problems_list[1]
+        print_problems(problems)
+        if check_answers:
+            print_answers(values_list, is_sum_list)
+    else:
+        raise Exception("Error: Wrong Parameters.")
+
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import arithmetic_arranger as aa
 
-sum1 = aa.ArithmeticArranger(["24134 + 40000", "14 + 1244123"])
+aa.arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+print()
+aa.arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+
